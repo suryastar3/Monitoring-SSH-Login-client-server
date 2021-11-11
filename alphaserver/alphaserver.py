98a09775fd9ff78fd85dfe37d2955755537d9fa9
suryastar3 <94125185+suryastar3@users.noreply.github.com>
2021-11-12 00:04:00 +0700

    Add files via upload

---
+++ alphaserver/alphaserver.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import socket
+import sys
+import traceback
+from threading import Thread
+
+
+def main():
+    start_server()
+
+
+def start_server():
+    host = '0.0.0.0'
+    port = 5000
+
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("Connection Create")
+
+    try:
+        soc.bind((host, port))
+    except:
+        print("Bind failed. Error : " + str(sys.exc_info()))
+        sys.exit()
+
+    # queue up to 5 requests
+    soc.listen(5)
+    print("Monitoring login running...")
+
+    # infinite loop- do not reset for every requests
+    while True:
+        connection, address = soc.accept()
+        ip, port = str(address[0]), str(address[1])
+        print("Received connection from " + ip + ":" + port)
+
+        try:
+            Thread(target=client_thread, args=(connection, ip, port)).start()
+        except:
+            print("Thread did not start.")
+            traceback.print_exc()
+
+    soc.close()
+
+
+def client_thread(connection, ip, port, max_buffer_size = 5120):
+    is_active = True
+
+    while is_active:
+        client_input = receive_input(connection, max_buffer_size)
+
+        if "--QUIT--" in client_input:
+            print("Client is requesting to quit")
+            connection.close()
+            print("Connection " + ip + ":" + port + " closed")
+            is_active = False
+        else:
+            print("Processed result: {}".format(client_input))
+            connection.sendall("-".encode("utf8"))
+
+
+def receive_input(connection, max_buffer_size):
+    client_input = connection.recv(max_buffer_size)
+    client_input_size = sys.getsizeof(client_input)
+
+    if client_input_size > max_buffer_size:
+        print("The input size is greater than expected {}".format(client_input_size))
+
+    decoded_input = client_input.decode("utf8").rstrip()
+    result = str(decoded_input)
+
+    return result
+
+if __name__ == "__main__":
+    main()
